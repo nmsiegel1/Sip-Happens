f37d7f25153b7cc7b3402c8fd93cfa1648fcfd6f
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-07-21 19:28:33 -0400

    connected models to index js

---
+++ models/index.js
@@ -0,0 +1,30 @@
+const User = require('./User');
+const Vote = require('./Vote');
+const Comment = require('./Comment');
+
+// associations
+
+Vote.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL',
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id',
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL',
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL',
+});
+
+module.exports = { User, Vote, Comment };
